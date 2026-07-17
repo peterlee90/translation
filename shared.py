222de7e465b7618d0cf00a22fbe77522c158dbf9
peterlee90 <peterlee90@users.noreply.github.com>
2026-07-17 17:27:02 +0900

    Update shared.py

--- shared.py
+++ shared.py
@@ -2,5 +2,6 @@ from qdrant_client import QdrantClient
 from sentence_transformers import SentenceTransformer
 
 # 딥러닝 모델 및 인메모리 DB 단일 인스턴스화
+EXACT_MATCH_DICT = {}
 embed_model = SentenceTransformer("BAAI/bge-small-en-v1.5", device="cuda")
 qdrant = QdrantClient(":memory:")
